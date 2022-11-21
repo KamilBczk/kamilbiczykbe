ec11ff656b4909dcb4ddbf7f63cc1a08d8810758
Kamil Biczyk <kamil.biczyk.pro@gmail.com>
2022-11-21 12:29:58 +0100

    http ==> https redirection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import HttpsRedirect from 'react-https-redirect';
 import './index.css';
 
 import Home from './views/home/Home';
@@ -14,19 +15,21 @@ import TestContact from './views/contact/TestContact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={<Navigate to="/home"/>} />
+  <HttpsRedirect>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/home"/>} />
 
-      <Route exact path="/home" element={<Home/>} />
-      <Route exact path="/about" element={<About/>} />
-      <Route exact path="/portfolio" element={<Portfolio />} />
-      <Route exact path="/project/:project" element={<Projects />} />
-      <Route exact path="/blog" element={<Blog />} />
-      <Route exact path="/contact" element={<Contact />} />
-      <Route exact path="/contacttest" element={<TestContact />} />
-    </Routes>
-  </BrowserRouter>
+        <Route exact path="/home" element={<Home/>} />
+        <Route exact path="/about" element={<About/>} />
+        <Route exact path="/portfolio" element={<Portfolio />} />
+        <Route exact path="/project/:project" element={<Projects />} />
+        <Route exact path="/blog" element={<Blog />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/contacttest" element={<TestContact />} />
+      </Routes>
+    </BrowserRouter>
+  </HttpsRedirect>
 );
 
 // If you want to start measuring performance in your app, pass a function
